09f841bdb64d88e86cb83d14c40701107a67bcb3
raulsposito <raulsposito@gmail.com>
2021-01-19 00:28:53 -0300

    clear MainContainer by moving navigation to navBar

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Logout from './Logout'
 
 const MainContainer = () => {
   return (
     <div className="MainContainer">
-        <Link to="/jobs/new">Find a Hero!</Link><br></br>
-        <Link to="/jobs">Be a Hero!</Link>
-        <Logout />
+
     </div>
   )
 }
